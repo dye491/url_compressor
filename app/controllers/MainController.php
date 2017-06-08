3c1205539bda07a8a65660c1078a350eb1a535fd
dye491 <dye491@gmail.com>
2017-06-08 18:57:42 +0200

    redirect implementation

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,6 +9,7 @@
 namespace app\controllers;
 
 
+use app\helpers\HashValidator;
 use app\helpers\RandomHelper;
 use app\helpers\UrlValidator;
 use app\models\Main;
@@ -34,6 +35,7 @@ class MainController
                 $hash = $model->findByUrl($_POST['url']);
                 if (!$hash) {
                     $hash = RandomHelper::generateRandomString(self::HASH_LEN);
+                    $model->add($_POST['url'], $hash);
                 }
                 $short_url = 'http://' . $_SERVER['HTTP_HOST'] . '/?h=' . $hash;
             } else {
@@ -50,9 +52,24 @@ class MainController
         $view->render(APP . '/views/main/index.php', $this->vars);
     }
 
-    public function viewAction()
+    public function actionView()
     {
-
+        if (isset($_GET['h'])) {
+            if (HashValidator::validate($_GET['h'])) {
+                $model = new Main();
+                if ($url = $model->findByHash($_GET['h'])) {
+                    header("Location: $url");
+                } else {
+                    http_response_code(404);
+                    echo "Ошибка (#404): Страница не найдена";
+                }
+            } else {
+                echo "Недопустимый символ в параметре h";
+            }
+        } else {
+            http_response_code(400);
+            echo "Ошибка (#400):Запрос не содержит параметра h";
+        }
     }
 
     public function set(array $vars)
